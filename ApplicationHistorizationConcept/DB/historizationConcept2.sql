960364caa6bcf6cacb5faf6b4a8c0beb2b1e2d5e
bogdan.moza <bogdan.moza@localhost>
2011-11-25 16:23:31 +0200

    <root>/ApplicationHistorizationConcept implement the adf test application for the custom and flashback historization model

--- ApplicationHistorizationConcept/DB/historizationConcept2.sql
+++ ApplicationHistorizationConcept/DB/historizationConcept2.sql
@@ -1,19 +1,6 @@
-SELECT t1.s as t1_s,
-  t1.e as t1_e,
-  t1.LAST_NAME,
-  t2.s as t2_s,
-  t2.e as t2_e,
-  t2.JOB_TITLE,
-  t3.s as t3_s,
-  t3.e as t3_e,
-  t3.DEPARTMENT_NAME
+SELECT 
+  t.*
 FROM
-  EMPLOYEES_HIST2 t1,
-  JOBS_HIST2 t2,
-  DEPARTMENTS_HIST2 t3
-WHERE t1.JOB_ID = t2.JOB_ID
-AND t1.DEPARTMENT_ID = t3.DEPARTMENT_ID
-AND (t1.s=t1.e OR t2.s=t2.e OR (t1.s=t2.s AND t1.e=t2.e) OR (t2.s < t1.e AND t2.e > t1.s) OR (t1.s < t2.e AND t1.e > t2.s))
-AND (t1.s=t1.e OR t3.s=t3.e OR (t1.s=t3.s AND t1.e=t3.e) OR (t3.s < t1.e AND t3.e > t1.s) OR (t1.s < t3.e AND t1.e > t3.s))
-AND (t2.s=t2.e OR t3.s=t3.e OR (t2.s=t3.s AND t2.e=t3.e) OR (t3.s < t2.e AND t3.e > t2.s) OR (t2.s < t3.e AND t2.e > t3.s))
-AND t1.EMPLOYEE_ID = 100;
+  EMPLOYEES_DETAIL_HIST2 t
+WHERE t.EMPLOYEE_ID = 100
+--AND t1.e = binary_double_infinity
