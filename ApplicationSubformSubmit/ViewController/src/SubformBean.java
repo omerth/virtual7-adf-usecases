6bd515d4874721d28f7d14d6b5f9f735cf0042a6
cristian.bolat <cristian.bolat@localhost>
2012-05-03 11:52:39 +0300

    <root>/ApplicationTaskSubformSubmit is an application that demonstrates the different ways subforms can be nested inside eachother

---
+++ ApplicationSubformSubmit/ViewController/src/SubformBean.java
@@ -0,0 +1,107 @@
+import javax.faces.event.ActionEvent;
+
+public class SubformBean {
+    private String input1;
+    private String input2;
+    private String input3;
+    private String input4;
+    private String input5;
+    private String input6;
+    private String input7;
+    private String input8;
+    private String input9;
+
+    public SubformBean() {
+    }
+
+    public void setInput1(String input1) {
+        this.input1 = input1;
+        System.out.println("Value 1 submitted: " + input1);
+
+    }
+
+    public String getInput1() {
+        return input1;
+    }
+
+    public void setInput2(String input2) {
+        this.input2 = input2;
+        System.out.println("Value 2 submitted: " + input2);
+
+    }
+
+    public String getInput2() {
+        return input2;
+    }
+
+
+    public void setInput3(String input3) {
+        this.input3 = input3;
+        System.out.println("Value 3 submitted: " + input3);
+
+    }
+
+    public String getInput3() {
+        return input3;
+    }
+
+    public void setInput4(String input4) {
+        this.input4 = input4;
+        System.out.println("Value 4 submitted: " + input4);
+
+    }
+
+    public String getInput4() {
+        return input4;
+    }
+
+    public void setInput5(String input5) {
+        this.input5 = input5;
+        System.out.println("Value 5 submitted: " + input5);
+
+    }
+
+    public String getInput5() {
+        return input5;
+    }
+
+    public void setInput6(String input6) {
+        this.input6 = input6;
+        System.out.println("Value 6 submitted: " + input6);
+
+    }
+
+    public String getInput6() {
+        return input6;
+    }
+
+    public void setInput7(String input7) {
+        this.input7 = input7;
+        System.out.println("Value 7 submitted: " + input7);
+
+    }
+
+    public String getInput7() {
+        return input7;
+    }
+
+    public void setInput8(String input8) {
+        this.input8 = input8;
+        System.out.println("Value 8 submitted: " + input8);
+
+    }
+
+    public String getInput8() {
+        return input8;
+    }
+
+    public void setInput9(String input9) {
+        this.input9 = input9;
+        System.out.println("Value 9 submitted: " + input9);
+
+    }
+
+    public String getInput9() {
+        return input9;
+    }
+}
