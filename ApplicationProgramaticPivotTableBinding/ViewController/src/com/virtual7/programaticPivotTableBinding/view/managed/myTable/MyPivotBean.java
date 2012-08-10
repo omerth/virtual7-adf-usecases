66ef6d67d7e8cd7c6b6812f2f858580306045c79
cristian.bolat <cristian.bolat@localhost>
2012-08-10 15:37:25 +0300

    This application shows how to create a programmatic pivot table model and dynamicaly group columns on it

--- ApplicationProgramaticPivotTableBinding/ViewController/src/com/virtual7/programaticPivotTableBinding/view/managed/myTable/MyPivotBean.java
+++ ApplicationProgramaticPivotTableBinding/ViewController/src/com/virtual7/programaticPivotTableBinding/view/managed/myTable/MyPivotBean.java
@@ -1,29 +1,65 @@
 package com.virtual7.programaticPivotTableBinding.view.managed.myTable;
 
-import com.virtual7.programaticPivotTableBinding.view.managed.FilterSpec;
-import com.virtual7.programaticPivotTableBinding.view.managed.RowsetDataSource;
+import com.virtual7.programaticPivotTableBinding.view.managed.normal.FilterSpec;
+import com.virtual7.programaticPivotTableBinding.view.managed.normal.RowsetDataSource;
 import com.virtual7.util.view.ADFUtils;
+
+import com.virtual7.util.view.JSFUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
 
+import oracle.adf.model.binding.DCControlBindingDef;
 import oracle.adf.model.binding.DCIteratorBinding;
 
+import oracle.adf.view.faces.bi.component.pivotTable.UIPivotTable;
+import oracle.adf.view.faces.bi.model.DataModel;
+
+import oracle.adfdt.model.binding.BindingInfo;
+
+import oracle.adfinternal.view.faces.dvt.model.binding.pivotTable.ActivePivotTableModel;
+import oracle.adfinternal.view.faces.dvt.model.binding.pivotTable.FacesPivotTableBinding;
+
+import oracle.adfinternal.view.faces.dvt.model.binding.pivotTable.FacesPivotTableBindingDef;
+
+import oracle.binding.AttributeBinding;
+import oracle.binding.BindingContainer;
+import oracle.binding.ControlBinding;
+import oracle.binding.OperationBinding;
+
+import oracle.dss.dataView.LocalDataSource;
+import oracle.dss.util.CubeDataDirector;
 import oracle.dss.util.DataException;
+
+import oracle.dss.util.DataMap;
+import oracle.dss.util.DataSource;
+import oracle.dss.util.EdgeOutOfRangeException;
+import oracle.dss.util.LayerMetadataMap;
+
+import oracle.dss.util.LayerOutOfRangeException;
 
 import oracle.jbo.Row;
 
 public class MyPivotBean {
+    //All the comented out code belongs to an initial implementation that worked, but had to be changed due to the fact 
+    //there was a requirement for sums to be displayed in the pivot table; 
 
     private MyPivotModel dataModel;
+    private MyPivotModel dynamicDataModel;
     private Object[][] rowsetData = null;
     private String[] columnNames = null;
     private HashMap<String, FilterSpec> filterSpecs = new HashMap<String, FilterSpec>();
     private ArrayList<String> colLayout = new ArrayList<String>();
     private ArrayList<String> rowLayout = new ArrayList<String>();
     private ArrayList<String> dataAttributes = new ArrayList<String>();
+    private UIPivotTable pivotTable;
+    private String dataItemForPageDef = "Salary";
 
     public MyPivotBean() {
         super();
@@ -35,9 +71,20 @@ public class MyPivotBean {
                                                      new String[] {"row", "row", "data"}, 
                                                      new String[] {"string", "string", "double"}, 
                                                      "EmployeesView1Iterator");
-        System.out.println(buildRowsetString);
- 
         dataModel = new MyPivotModel(buildRowsetString, null);
+
+        OperationBinding op1 = ADFUtils.getBindingContainer().getOperationBinding("changeDynamicQuerry");
+        if (ADFUtils.getBindingContainer().getOperationBinding("changeDynamicQuerry") != null) {
+            OperationBinding op = ADFUtils.findOperation("changeDynamicQuerry");
+            Map map = op.getParamsMap();
+            map.put("querry", "select * from employees");
+            op.execute();
+        }
+        String dynamicRowsetString = buildRowsetString(new String[] { "DEPARTMENT_ID", "LAST_NAME", "SALARY" }, 
+                                                     new String[] {"row", "row", "data"}, 
+                                                     new String[] {"string", "string", "double"}, 
+                                                     "DynamicVO1Iterator");
+        dynamicDataModel = new MyPivotModel(dynamicRowsetString, null);
     }
     
     private String buildRowsetString(String[] colNames, String[] cols, String[] colTypes, String iteratorBinding) {
@@ -59,7 +106,9 @@ public class MyPivotBean {
     }
     
     private String buildRowsetData(String iteratorBinding, String[] attNames) {
+        if (ADFUtils.getBindingContainer().get(iteratorBinding) != null) {
         DCIteratorBinding iter = ADFUtils.findIterator(iteratorBinding);
+        //iter.executeQuery();
         Row[] allRowsInRange = iter.getAllRowsInRange();
         String data = "";
         for (int i = 0; i < allRowsInRange.length; i++) {
@@ -74,7 +123,10 @@ public class MyPivotBean {
             data = data + row;
         }
         return data;
+        }
+        return "";
     }
+    
     
 //    private Object[][] buildRowsetData(String iteratorBinding, String[] attNames) {
 //        DCIteratorBinding iter = ADFUtils.findIterator(iteratorBinding);
@@ -181,4 +233,71 @@ public class MyPivotBean {
 //        colLayout.add("MeasDim");
 //        dataAttributes.add("Salary");
 //    }
+
+    public void generateVO(ActionEvent actionEvent) throws EdgeOutOfRangeException, LayerOutOfRangeException {
+        OperationBinding op = ADFUtils.findOperation("changeDynamicQuerry");
+        Map map = op.getParamsMap();
+        map.put("querry", "select * from employees");
+        op.execute();    
+    }
+
+    public void dynamicValueChanged(ValueChangeEvent valueChangeEvent) {
+        if ("0".equals(valueChangeEvent.getNewValue())) {
+            String buildRowsetString = buildRowsetString(new String[] { "DEPARTMENT_ID", "LAST_NAME", "SALARY" }, 
+                                                         new String[] {"row", "row", "data"}, 
+                                                         new String[] {"string", "string", "double"}, 
+                                                         "DynamicVO1Iterator");
+            System.out.println(buildRowsetString);
+            
+            dynamicDataModel = new MyPivotModel(buildRowsetString, null);
+            dynamicDataModel.getRowsetDataSource().setTotalsEnabled(false);
+        } else if ("1".equals(valueChangeEvent.getNewValue())) {
+            String buildRowsetString = buildRowsetString(new String[] { "JOB_ID", "LAST_NAME", "SALARY" }, 
+                                             new String[] {"row", "row", "data"}, 
+                                             new String[] {"string", "string", "double"}, 
+                                             "DynamicVO1Iterator");
+            System.out.println(buildRowsetString);
+        
+            dynamicDataModel = new MyPivotModel(buildRowsetString, null);
+            dynamicDataModel.getRowsetDataSource().setTotalsEnabled(false);
+        } else if ("2".equals(valueChangeEvent.getNewValue())) {
+            String buildRowsetString = buildRowsetString(new String[] { "DEPARTMENT_ID", "LAST_NAME", "SALARY" }, 
+                                                         new String[] {"row", "row", "data"}, 
+                                                         new String[] {"string", "string", "double"}, 
+                                                         "DynamicVO1Iterator");
+            System.out.println(buildRowsetString);
+            
+            dynamicDataModel = new MyPivotModel(buildRowsetString, null);
+            dynamicDataModel.getRowsetDataSource().setTotalsEnabled(true);
+        } else {
+            String buildRowsetString = buildRowsetString(new String[] { "JOB_ID", "LAST_NAME", "SALARY" }, 
+                                             new String[] {"row", "row", "data"}, 
+                                             new String[] {"string", "string", "double"}, 
+                                             "DynamicVO1Iterator");
+            System.out.println(buildRowsetString);
+            
+            dynamicDataModel = new MyPivotModel(buildRowsetString, null);
+            dynamicDataModel.getRowsetDataSource().setTotalsEnabled(true);
+        }
+    }
+
+    public MyPivotModel getDynamicDataModel() {
+        return dynamicDataModel;
+    }
+
+    public void setPivotTable(UIPivotTable pivotTable) {
+        this.pivotTable = pivotTable;
+    }
+
+    public UIPivotTable getPivotTable() {
+        return pivotTable;
+    }
+
+    public void setDataItemForPageDef(String dataItemForPageDef) {
+        this.dataItemForPageDef = dataItemForPageDef;
+    }
+
+    public String getDataItemForPageDef() {
+        return dataItemForPageDef;
+    }
 }
