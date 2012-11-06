e1efab9f96cca148db5e0c8dd075107713ebb10c
cristian.bolat <cristian.bolat@localhost>
2012-11-06 15:11:30 +0200

    Application ADF Development Pattern is an application that demonstrates the implementation of the pattern used in development of future ADF applications for the virtual7 team.

--- ApplicationADFDevelopmentPattern/Model/src/com/virtual7/adfDevelopmentPattern/model/context/ADFPatternChangeContext.java
+++ ApplicationADFDevelopmentPattern/Model/src/com/virtual7/adfDevelopmentPattern/model/context/ADFPatternChangeContext.java
@@ -1,11 +1,26 @@
 package com.virtual7.adfDevelopmentPattern.model.context;
 
+
 import com.virtual7.util.model.context.AChangeContext;
+
+import oracle.jbo.domain.Timestamp;
 
 public class ADFPatternChangeContext extends AChangeContext {
     private static final long serialVersionUID = 4684697966515426645L;
 
+    public static final String JOB_ID = "ST_MAN";
+
+    private Timestamp hireDate;
+
     public ADFPatternChangeContext() {
         super();
+    }
+
+    public void setHireDate(Timestamp hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Timestamp getHireDate() {
+        return hireDate;
     }
 }
