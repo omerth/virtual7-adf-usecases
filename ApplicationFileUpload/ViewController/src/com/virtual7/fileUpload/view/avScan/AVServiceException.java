0a1084b4cd4044d33277f84e31be53a83a9635a4
sadorian <sadorian@localhost>
2013-01-08 13:12:03 +0200

    Integrate AV scan

---
+++ ApplicationFileUpload/ViewController/src/com/virtual7/fileUpload/view/avScan/AVServiceException.java
@@ -0,0 +1,11 @@
+package com.virtual7.fileUpload.view.avScan;
+
+import java.io.IOException;
+
+public class AVServiceException extends Exception {
+
+	public AVServiceException(IOException e) {
+		super(e);
+	}
+
+}
